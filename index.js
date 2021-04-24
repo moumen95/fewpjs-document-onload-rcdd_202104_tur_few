181041bdeb7bef5ce90e7d3ed5af579ee0cb14de
Moumen Hallak <moumen95@gmail.com>
2021-04-24 00:58:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+// // Your code goes here
+
+// function domLoading(){
+//   document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+
+//   const load = document.createEvent('load');
+//   event.initEvent( 'DOMContentLoaded', true, true );
+//   window.document.dispatchEvent,(load) =>{
+//   document.querySelector( 'p' ).textContent = "This is really cool!";
+//   })
+// }
+
+document.addEventListener("DOMContentLoaded", function(e){
+    document.querySelector( 'p' ).textContent = "This is really cool!";
+})

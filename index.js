a2276ba0211d300b3437ad2453faf846402cd472
Moumen Hallak <moumen95@gmail.com>
2021-04-24 00:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 function domLoading(){
   document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
-}
-  const event = document.createEvent( 'Event' );
+
+  const event = document.createEvent('load');
   event.initEvent( 'DOMContentLoaded', true, true );
   window.document.dispatchEvent( event );
   document.querySelector( 'p' ).textContent = "This is really cool!";
-
+}
 
 // document.addEventListener("DOMContentLoaded", function(){
 //     console.log("This is really cool!");

13ec6734c46b3cb1bd775062b8ec7ee3d7467160
Moumen Hallak <moumen95@gmail.com>
2021-04-24 00:46:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,18 +6,13 @@ function domLoading(){
   const event = document.createEvent( 'Event' );
   event.initEvent( 'DOMContentLoaded', true, true );
   window.document.dispatchEvent( event );
+  document.querySelector( 'p' ).textContent = "This is really cool!";
 
 
+// document.addEventListener("DOMContentLoaded", function(){
+//     console.log("This is really cool!");
+// });
+//     console.log("This is really cool!");
 
-document.addEventListener("DOMContentLoaded", function(){
-    console.log("This is really cool!");
-});
-    console.log("This is really cool!");
+//   document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
 
-  document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
-
-
-
-    expect( document.querySelector( 'p' )
-        .textContent )
-      .to.equal( "This is really cool!" );

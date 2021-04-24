7b60a34103611caa89b92c0fe934f3b3652c237c
Moumen Hallak <moumen95@gmail.com>
2021-04-24 00:38:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Your code goes here
 
-document.addEventListener("DOMContentLoaded", ready);
+document.addEventListener("DOMContentLoaded");
 document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
